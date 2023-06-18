7b809b83b0fe8aae4a6f7d1d8aed94c70656abe3
ceepu8 <caophuonguyen132002@gmail.com>
2023-06-18 20:59:07 +0700

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Messenger Clone",
-  description: "Messenger Clone",
+  title: "Messenger",
+  description: "Messenger",
 };
 
 export default function RootLayout({
